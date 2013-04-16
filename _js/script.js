0c26fe030194c62ad356f4df9ee5f8f2e244d57d
Grant Staublin <grant.staublin@gmail.com>
2013-04-16 17:55:36 -0400

    Added working version of fixed activity-nav behavior

--- _js/script.js
+++ _js/script.js
@@ -15,11 +15,60 @@ $('.sidebar-mod a').click(function (e) {
 
 
 
+// fixed header
+// function sticky_header() {
+// 	var window_top = $(window).scrollTop();
+// 	var header_top = $('.activity-nav-mod').offset().top;
+// 	if (window_top > header_top) {
+// 		$(header_top).addClass('fixed');
+// 	} else {
+// 		$(header_top).removeClass('fixed');
+// 	}
+	
+
+// };
 
 
 
 
 
+
+
+
+// $(document).ready(function() {
+// 	var window_top = $(window).scrollTop();
+// 	var header_top = $('.activity-nav-mod');
+
+// 	if ( window_top > header_top ) {
+// 		$('.activity-nav-mod').addClass('fixed');
+// 		console.log('fixie');
+// 	} else {
+// 		$('activity-nav-mod').removeClass('fixed');
+// 		console.log('no fixie');
+// 	}
+
+// 	console.log('yo yolo yo');
+
+// }); 
+// end document.ready
+
+
+$(document).ready(function(){
+	var window_top = $(window).scrollTop();
+	var header_top = $('.activity-nav-mod').offset().top;
+
+	$(window).scroll(function(){
+
+		if( $(window).scrollTop() > header_top ) {
+			$('.activity-nav-mod').addClass('fixed');
+			console.log('fixed');
+		} else {
+			$('.activity-nav-mod').removeClass('fixed');
+			console.log('no fixie');
+		}
+	});
+	console.log('yo yolo');
+});
 
 
 
