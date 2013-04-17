ab2f4c784dd37b6edb15ef0eb193e660e1d1c533
Grant Staublin <grant.staublin@gmail.com>
2013-04-17 17:56:00 -0400

    Working version of sticky interactives for 6040 and 7030

--- _js/script.js
+++ _js/script.js
@@ -26,11 +26,12 @@ $(document).ready(function(){
 	// var window_top = $(window).scrollTop();
 	// var header_top = $('.activity-nav-mod').offset().top;
 	var header_top = $('.content-hdr').offset().top;
+	var interactive_top = $('.interactive-mod').offset().top;
 	console.log(header_top);
 
 	$(window).scroll(function(){
 		var window_top = $(window).scrollTop();
-		console.log(window_top);
+		// console.log(window_top);
 
 
 		if( window_top >= header_top/2 ) {
@@ -44,9 +45,31 @@ $(document).ready(function(){
 			$('.content-hdr').removeClass('extra-pad');
 			$('.activity-nav-mod .activity-nav-logo .h2').removeClass('visible');
 		}
+
+		// if ( window_top >= interactive_top - 50 ) {
+		// 	$('.interactive').css('background-color', 'red');
+		// 	$('.interactive').addClass('interactive-fixed');
+		// } else {
+		// 	$('.interactive').css('background-color', 'yellow');
+		// 	$('.interactive').removeClass('interactive-fixed');
+		// };
 	});
+
+	// interactive sticky
+	$('.interactive-mod').waypoint('sticky', {
+		offset: 100
+	});
+
 });
 
+
+// $(document).ready(function(){
+// 	$('.interactive-mod').waypoint('sticky', {
+// 		offset: 72
+// 	});
+// 	console.log('sticky');
+
+// });
 
 
 
